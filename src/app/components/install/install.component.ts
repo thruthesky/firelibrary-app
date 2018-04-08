497605bc10a35d40c6f06631bdeeca3adeac9bb5
JaeHo Song <thruthesky@gmail.com>
2018-04-08 23:52:55 +0800

    update on loign. install

--- src/app/components/install/install.component.ts
+++ src/app/components/install/install.component.ts
@@ -8,22 +8,38 @@ import { FireService } from './../../modules/firelibrary/core';
 })
 export class InstallComponent implements OnInit {
 
-
+  show = {
+    loader: false,
+    content: false,
+    installed: false
+  };
   constructor(
     public fire: FireService
   ) {
+    this.show.loader = true;
 
+    fire.checkInstall().then(re => {
+      this.show.loader = false;
+      this.show.content = true;
+      this.show.installed = re.data.installed;
+    })
+      .catch(e => {
+        this.show.loader = false;
+        alert(e.message);
+      });
   }
 
   ngOnInit() {
+    //
   }
-
 
   onSubmitInstall(event: Event) {
     event.preventDefault();
     console.log(`Going to set ${this.fire.user.email} as admin`);
     this.fire.install({ email: this.fire.user.email }).then(re => {
       console.log('install: ', re);
+      this.fire.checkInstall()
+        .then( result => this.show.installed = result.data.installed );
     })
       .catch(e => alert(e.message));
     return false;
